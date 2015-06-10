6c48deb58e4e6c991dd979109b0c56a798e3f205
원준 <@main-laptop>
2015-06-10 13:20:09 +0900

    MemberDAOImpl 메서드에 설명 주석 추가.

--- gibuticon/src/main/java/org/kosta/gibuticon/model/member/MemberDAOImpl.java
+++ gibuticon/src/main/java/org/kosta/gibuticon/model/member/MemberDAOImpl.java
@@ -12,6 +12,10 @@ public class MemberDAOImpl implements MemberDAO {
 	/* (non-Javadoc)
 	 * @see org.kosta.gibuticon.model.member.MemberDAO#registerMember(org.kosta.gibuticon.model.member.MemberVO)
 	 */
+	/**
+	 * 회원가입시 id,password,name,address,tel,birth,email 정보를 
+	 * MemberVO로 받아와 member table에 등록.
+	 */
 	@Override
 	public void registerMember(MemberVO memberVO) {
 		System.out.println("dao: "+memberVO);
@@ -20,6 +24,9 @@ public class MemberDAOImpl implements MemberDAO {
 
 	/* (non-Javadoc)
 	 * @see org.kosta.gibuticon.model.member.MemberDAO#findMemberById(java.lang.String)
+	 */
+	/**
+	 * member table에서 id에 일치하는 회원 정보를 반환.
 	 */
 	@Override
 	public MemberVO findMemberById(String id) {
@@ -31,6 +38,10 @@ public class MemberDAOImpl implements MemberDAO {
 	/* (non-Javadoc)
 	 * @see org.kosta.gibuticon.model.member.MemberDAO#updateMember(org.kosta.gibuticon.model.member.MemberVO)
 	 */
+	/**
+	 * id에 해당하는 회원정보의 password, address, tell, email정보를 수정.
+	 * MemberVO 객체로 id와 수정 정보를 묶어서 받는다.
+	 */
 	@Override
 	public void updateMember(MemberVO memberVO) {
 		sqlSessionTemplate.update("member.updateMember", memberVO);
@@ -38,6 +49,9 @@ public class MemberDAOImpl implements MemberDAO {
 
 	/* (non-Javadoc)
 	 * @see org.kosta.gibuticon.model.member.MemberDAO#deleteMemberById(java.lang.String)
+	 */
+	/**
+	 * id에 해당하는 회원정보를 삭제.
 	 */
 	@Override
 	public void deleteMemberById(String id) {
@@ -47,6 +61,9 @@ public class MemberDAOImpl implements MemberDAO {
 	/* (non-Javadoc)
 	 * @see org.kosta.gibuticon.model.member.MemberDAO#giveAdminRight(java.lang.String)
 	 */
+	/**
+	 * id에 일치하는 회원정보의 관리자 권한(admin column)을 yes로 수정.
+	 */
 	@Override
 	public void giveAdminRight(String id){
 		sqlSessionTemplate.update("member.giveAdminRight",id);
@@ -54,6 +71,10 @@ public class MemberDAOImpl implements MemberDAO {
 	
 	/* (non-Javadoc)
 	 * @see org.kosta.gibuticon.model.member.MemberDAO#login(org.kosta.gibuticon.model.member.MemberVO)
+	 */
+	/**
+	 * id와 password 정보를 MemberVO객체로 받아와 
+	 * id, password가 모두 일치하는 회원의 정보를 반환. 
 	 */
 	@Override
 	public MemberVO login(MemberVO memberVO){
