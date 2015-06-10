1cc012f41025a9aa142e14e3e090f0ab7d2aa8f3
원준 <@main-laptop>
2015-06-10 13:58:27 +0900

    injection error 수정. root-context.xml에서 model 명시 삭제 (service bean도 생성하기 위해) controller service class에 annotation 명시

--- gibuticon/src/main/java/org/kosta/gibuticon/service/MemberServiceImpl.java
+++ gibuticon/src/main/java/org/kosta/gibuticon/service/MemberServiceImpl.java
@@ -1,56 +1,85 @@
 package org.kosta.gibuticon.service;
 
+import javax.annotation.Resource;
+
 import org.kosta.gibuticon.model.member.MemberDAO;
 import org.kosta.gibuticon.model.member.MemberVO;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberServiceImpl implements MemberService {
+	@Resource
 	private MemberDAO memberDAO;
-	
-	/* (non-Javadoc)
-	 * @see org.kosta.gibuticon.service.MemberService#registerMember(org.kosta.gibuticon.model.member.MemberVO)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.kosta.gibuticon.service.MemberService#registerMember(org.kosta.gibuticon
+	 * .model.member.MemberVO)
 	 */
 	@Override
-	public void registerMember(MemberVO memberVO){
+	public void registerMember(MemberVO memberVO) {
 		memberDAO.registerMember(memberVO);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.kosta.gibuticon.service.MemberService#findMemberById(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.kosta.gibuticon.service.MemberService#findMemberById(java.lang.String
+	 * )
 	 */
 	@Override
-	public MemberVO findMemberById(String id){
+	public MemberVO findMemberById(String id) {
 		return memberDAO.findMemberById(id);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.kosta.gibuticon.service.MemberService#updateMember(org.kosta.gibuticon.model.member.MemberVO)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.kosta.gibuticon.service.MemberService#updateMember(org.kosta.gibuticon
+	 * .model.member.MemberVO)
 	 */
 	@Override
-	public void updateMember(MemberVO memberVO){
+	public void updateMember(MemberVO memberVO) {
 		memberDAO.updateMember(memberVO);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.kosta.gibuticon.service.MemberService#deleteMemberById(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.kosta.gibuticon.service.MemberService#deleteMemberById(java.lang.
+	 * String)
 	 */
 	@Override
-	public void deleteMemberById(String id){
+	public void deleteMemberById(String id) {
 		memberDAO.deleteMemberById(id);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.kosta.gibuticon.service.MemberService#giveAdminRight(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.kosta.gibuticon.service.MemberService#giveAdminRight(java.lang.String
+	 * )
 	 */
 	@Override
-	public void giveAdminRight(String id){
+	public void giveAdminRight(String id) {
 		memberDAO.giveAdminRight(id);
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.kosta.gibuticon.service.MemberService#login(org.kosta.gibuticon.model.member.MemberVO)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.kosta.gibuticon.service.MemberService#login(org.kosta.gibuticon.model
+	 * .member.MemberVO)
 	 */
 	@Override
-	public MemberVO login(MemberVO memberVO){
+	public MemberVO login(MemberVO memberVO) {
 		return memberDAO.login(memberVO);
 	}
 }
