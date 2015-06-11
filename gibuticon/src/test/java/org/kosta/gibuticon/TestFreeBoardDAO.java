2999be500af2ef5568a978a6337bbe108e74eda9
Ji <Ji@JiMonster>
2015-06-11 13:32:09 +0900

    freeboarddao

--- gibuticon/src/test/java/org/kosta/gibuticon/TestFreeBoardDAO.java
+++ gibuticon/src/test/java/org/kosta/gibuticon/TestFreeBoardDAO.java
@@ -2,7 +2,7 @@ package org.kosta.gibuticon;
 
 import java.sql.Date;
 
-import org.kosta.gibuticon.model.board.FreeBoardDAOImpl;
+import org.kosta.gibuticon.model.board.FreeBoardDAO;
 import org.kosta.gibuticon.model.board.FreeBoardVO;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -10,7 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestFreeBoardDAO {
 	public static void main(String[] args) {
 	    ApplicationContext ctx = new ClassPathXmlApplicationContext("root-context.xml");
-		FreeBoardDAOImpl dao = (FreeBoardDAOImpl) ctx.getBean("freeBoardDao");
+		FreeBoardDAO dao = (FreeBoardDAO) ctx.getBean("freeBoardDao");
 		System.out.println(dao.getFreeBoardList());
 		System.out.println(dao.getFreeBoardByNo("2"));
 //		dao.writeFreeBoard(new FreeBoardVO("hagisiro","sirosjro","chocamp"));
