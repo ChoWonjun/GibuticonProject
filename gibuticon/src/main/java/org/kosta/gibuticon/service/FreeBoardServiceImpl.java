7a9f919e9628a89f12c501cf46720eb3d020f3d2
You kyounghee <You kyounghee@kyunghee>
2015-06-13 11:11:18 +0900

    freeboardServiceImpl 다시 추가

---
+++ gibuticon/src/main/java/org/kosta/gibuticon/service/FreeBoardServiceImpl.java
@@ -0,0 +1,62 @@
+package org.kosta.gibuticon.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.kosta.gibuticon.model.free.FreeBoardDAO;
+import org.kosta.gibuticon.model.free.FreeBoardVO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FreeBoardServiceImpl implements FreeBoardService {
+	@Resource(name="freeBoardDAOImpl")
+	private FreeBoardDAO freeBoardDAO;
+	
+	/* (non-Javadoc)
+	 * @see org.kosta.gibuticon.service.FreeBoardService#writeFreeBoard(org.kosta.gibuticon.model.board.FreeBoardVO)
+	 */
+	@Override
+	public void writeFreeBoard(FreeBoardVO freeBoardVO){
+		freeBoardDAO.writeFreeBoard(freeBoardVO);
+	}
+
+	@Override
+	public List<FreeBoardVO> getFreeBoardList(String pageNo) {
+		return freeBoardDAO.getFreeBoardList(pageNo);
+	}
+
+	@Override
+	public FreeBoardVO getFreeBoardByNo(String no) {
+		//조회수 증가
+		freeBoardDAO.updateHit(no);
+		return freeBoardDAO.getFreeBoardByNo(no);
+	}
+
+	@Override
+	public void updateFreeBoard(FreeBoardVO freeBoardVO) {
+		freeBoardDAO.updateFreeBoard(freeBoardVO);
+	}
+
+	@Override
+	public void deleteFreeBoard(String no) {
+		freeBoardDAO.deleteFreeBoard(no);
+		
+	}
+
+	@Override
+	public String getPageNo(String no) {
+		return freeBoardDAO.getPageNo(no);
+	}
+
+	@Override
+	public int getTotalPostingCount() {
+		return freeBoardDAO.getTotalPostingCount();
+	}
+
+	@Override
+	public FreeBoardVO getFundByNoNotHit(String no) {
+		return freeBoardDAO.getFreeBoardByNo(no);
+	}
+
+}
