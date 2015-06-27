72eecbdb004d0b8c04c615c56282d09317d8b4d9
Ji <Ji@JiMonster>
2015-06-27 13:08:24 +0900

    controller

--- gibuticon-ver2/src/main/java/org/kosta/gibuticon/controller/NoticeController.java
+++ gibuticon-ver2/src/main/java/org/kosta/gibuticon/controller/NoticeController.java
@@ -7,12 +7,8 @@ import javax.annotation.Resource;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-
 
 import org.kosta.gibuticon.model.member.LoginCheck;
-import org.kosta.gibuticon.model.member.MemberVO;
 import org.kosta.gibuticon.model.notice.ListVO;
 import org.kosta.gibuticon.model.notice.NoticeVO;
 import org.kosta.gibuticon.model.notice.PagingBean;
@@ -27,11 +23,11 @@ import org.springframework.web.servlet.ModelAndView;
 public class NoticeController {
 	@Resource
 	private NoticeService noticeService;
-	private Logger log = LoggerFactory.getLogger(getClass());	
-	
+	private Logger log = LoggerFactory.getLogger(getClass());
+
 	/**
-	 * 글쓰기 입력폼을 불러오는 컨트롤러
-	 * notice 폴더에 있는 write.jsp로 보낸다
+	 * 글쓰기 입력폼을 불러오는 컨트롤러 notice 폴더에 있는 write.jsp로 보낸다
+	 * 
 	 * @param request
 	 * @return
 	 */
@@ -40,9 +36,10 @@ public class NoticeController {
 	public String noticeWriteView(HttpServletRequest request) {
 		return "notice_write";
 	}
-	
+
 	/**
 	 * 공지사항에 글을 쓴거를 데이터 베이스로 넘기는 컨트롤러
+	 * 
 	 * @param noticeVO
 	 * @return
 	 */
@@ -53,7 +50,7 @@ public class NoticeController {
 		noticeService.write(noticeVO);
 		return new ModelAndView("redirect:getList.gibu");
 	}
-	
+
 	/**
 	 * 공지사항 목록글들을 불러오는 컨트롤러 
 	 * @param pageNo
@@ -85,7 +82,7 @@ public class NoticeController {
 			//System.out.println(map.get("input"));
 			list = noticeService.getListBySearchingTitle(map);
 		} else if (searchCondition == "1") { // 내용만
-			map.put("content", input);
+			map.put("input", input);
 			list = noticeService.getListBySearchingContent(map);
 		} else if (searchCondition == "2") { // 제목+내용
 
@@ -101,9 +98,10 @@ public class NoticeController {
 		return new ModelAndView("notice_list", "nlvo", lvo);
 	}
 
-	
+
 	/**
 	 * 공지사항의 글을 클릭 할 시 글의 컨텐츠(내용)을 불러오는 컨트롤러
+	 * 
 	 * @param noticeNo
 	 * @param request
 	 * @param response
@@ -143,25 +141,23 @@ public class NoticeController {
 		// System.out.println(vo);
 		return new ModelAndView("notice_show_content", "posting", vo);
 	}
-	
-	
+
 	/**
-	 *  공지사항의 글을 지우기위한 delete 컨트롤러
+	 * 공지사항의 글을 지우기위한 delete 컨트롤러
 	 * 
 	 * @param noticeNo
 	 * @return
 	 */
 	@RequestMapping("notice/delete.gibu")
 	@LoginCheck
-	public String delete(String noticeNo){
+	public String delete(String noticeNo) {
 		noticeService.delete(noticeNo);
 		return "redirect:getList.gibu";
 	}
-	
-	
+
 	/**
-	 *  업데이트 폼을 불러오기 위한 컨트롤러
-	 * notice 폴더에 있는 update 폼을 불러온다
+	 * 업데이트 폼을 불러오기 위한 컨트롤러 notice 폴더에 있는 update 폼을 불러온다
+	 * 
 	 * @param noticeNo
 	 * @return
 	 */
@@ -172,9 +168,10 @@ public class NoticeController {
 		NoticeVO nvo = noticeService.getNoticeByNo(noticeNo);
 		return new ModelAndView("notice_update", "nvo", nvo);
 	}
-	
+
 	/**
 	 * 업데이트한 내용을 실제 데이터베이스로 보내는 컨트롤러
+	 * 
 	 * @param noticeVO
 	 * @return
 	 */
