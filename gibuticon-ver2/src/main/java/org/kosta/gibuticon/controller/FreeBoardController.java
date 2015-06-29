6b90d341902375181ecb5ff988e3e9d0da870067
Ji <Ji@JiMonster>
2015-06-29 11:43:45 +0900

    freeboard 페이징

--- gibuticon-ver2/src/main/java/org/kosta/gibuticon/controller/FreeBoardController.java
+++ gibuticon-ver2/src/main/java/org/kosta/gibuticon/controller/FreeBoardController.java
@@ -82,6 +82,8 @@ public class FreeBoardController {
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("page", pageNo);
 		map.put("input", input);
+		map.put("searchSelect", searchSelect);
+
 		List<FreeBoardVO> list = null;
 
 		if (searchSelect.equals("0")) { // 제목만
