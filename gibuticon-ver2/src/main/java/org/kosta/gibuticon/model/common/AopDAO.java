67ca9c25f6d44aa2590bb50e67314cab737abc54
원준 <@main-laptop>
2015-06-30 14:27:08 +0900

    Aop 적용 에러페이지 추가

---
+++ gibuticon-ver2/src/main/java/org/kosta/gibuticon/model/common/AopDAO.java
@@ -0,0 +1,5 @@
+package org.kosta.gibuticon.model.common;
+
+public interface AopDAO {
+
+}
---
+++ gibuticon-ver2/src/main/java/org/kosta/gibuticon/model/common/AopDAOImpl.java
@@ -0,0 +1,14 @@
+package org.kosta.gibuticon.model.common;
+
+import javax.annotation.Resource;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AopDAOImpl implements AopDAO {
+	@Resource
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	
+}
